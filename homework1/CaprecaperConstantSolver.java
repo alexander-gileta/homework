cb191d992952d16b9c91fee2260ff491cd864ba7
alexander-gileta <alexander.gileta@yandex.ru>
2025-01-31 12:50:31 -0800

    Update CaprecaperConstantSolver.java

--- homework1/CaprecaperConstantSolver.java
+++ homework1/CaprecaperConstantSolver.java
@@ -1,52 +1,37 @@
 public class PalindromSolver {
 
-    static boolean isPalindrome(int n) {
-        String s = "" + n;
+    public static int countK(int number) {
+        if (number == 6174) {
+            return 0; 
+        }
 
-        if (s.length() % 2 == 0) {
-            for (int i = 0; i < s.length()/2; i++) {
-                if (s.charAt(i) != s.charAt(s.length()-i-1)) {
-                    return false;
-                }
-            }
-        } else {
-            for (int i = 0; i < (s.length()-1)/2; i++) {
-                if (s.charAt(i) != s.charAt(s.length()-i-1)) {
-                    return false;
-                }
+        
+        String numStr = String.valueOf(number);
+
+
+        String asc = sortDigits(numStr);
+        String desc = new StringBuilder(asc).reverse().toString();
+
+        int high = Integer.parseInt(desc);
+        int low = Integer.parseInt(asc);
+        int difference = high - low;
+
+        return 1 + countK(difference); 
+    }
+
+    private static boolean allDigitsSame(String str) {
+        char firstDigit = str.charAt(0);
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) != firstDigit) {
+                return false; 
             }
         }
         return true;
     }
 
-    public static boolean isPalindromeDescendant(int number) {
-        if (((int) Math.floor(Math.log10(number)) + 1) < 2) {
-            return false;
-        }
-
-        if (isPalindrome(number)) {
-            return true;
-        }
-
-        String s = "" + number;
-        StringBuilder str = new StringBuilder(); 
-
-        if (s.length() % 2 == 1) {
-            for (int i = 0; i < s.length()-1; i+=2) {
-                addSum(str, s, i);
-            }
-
-            str.append(s.charAt(s.length()-1));
-        } else {
-            for (int i = 0; i < s.length(); i+=2) {
-                addSum(str, s, i);
-            }
-        }
-
-        return isPalindromeDescendant(Integer.parseInt(str.toString()));
-    }
-
-    static void addSum(StringBuilder str, String s, int i) {
-        str.append(Integer.parseInt ("" + s.charAt(i)) + Integer.parseInt ("" + s.charAt(i+1)));
+    private static String sortDigits(String str) {
+        char[] digits = str.toCharArray();
+        java.util.Arrays.sort(digits); 
+        return new String(digits);
     }
 }
