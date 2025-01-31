6efec3a84e7b3025ddeee5034088e7c1c3d50222
alexander-gileta <alexander.gileta@yandex.ru>
2025-01-31 12:52:55 -0800

    Update CycleBitRotator.java

--- homework1/CycleBitRotator.java
+++ homework1/CycleBitRotator.java
@@ -1,16 +1,15 @@
 class KnightBoardSolver {
-    public static boolean knightBoardCapture(int[][] board) {
-      int n = board.length;
-  
-      for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-          if (board[i][j] == 1) {
-            if (isUnderAttack(board, i, j)) {
-              return false;
-            }
-          }
-        }
-      }
-      return true;
+    public static int rotateRight(int number, int shift) {
+      int bitCount = Integer.SIZE;
+      shift %= bitCount;
+
+      return (number >>> shift) | (number << (bitCount - shift));
+    }
+
+    public static int rotateLeft(int number, int shift) {
+      int bitCount = Integer.SIZE;
+      shift %= bitCount;
+
+      return (number << shift) | (number >>> (bitCount - shift));
     }
 }
